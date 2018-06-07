c19ee378c9b9ce24f2473fcb7068d55edfd31ee7
bachdx <d-bach@starx.co.jp>
2018-06-07 18:47:54 +0900

    init

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- Pageloader -->
+    <div class="pageloader"></div>
+    <!-- <div class="infraloader is-active"></div> -->
+    <navbar/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/partials/Navbar.vue'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+
+</script>
+
+<style>
+</style>
